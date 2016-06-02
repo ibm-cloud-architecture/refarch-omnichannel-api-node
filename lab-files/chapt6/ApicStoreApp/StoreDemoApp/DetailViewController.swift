1cc014453d7556c440e5e249a9192c1baf7c9350
Gang Chen <gangchen@us.ibm.com>
2016-06-02 17:53:14 -0500

    Integrate with APIC for Review

--- lab-files/chapt6/ApicStoreApp/StoreDemoApp/DetailViewController.swift
+++ lab-files/chapt6/ApicStoreApp/StoreDemoApp/DetailViewController.swift
@@ -22,13 +22,16 @@ class DetailViewController: UIViewController, UINavigationControllerDelegate, UI
     
     @IBOutlet var reviewTable: UITableView!
     
+    var http: Http!
+    
     var item: Item!
    // var item: Item = Item(name: "Dayton Meat Chopper", desc: "Punched-card tabulating machines and time clocks were not the only products offered by the young IBM. Seen here in 1930, manufacturing employees of IBM's Dayton Scale Company are assembling Dayton Safety Electric Meat Choppers. These devices, which won the Gold Medal at the 1926 Sesquicentennial International Exposition in Philadelphia, were produced in both counter base and pedestal styles (5000 and 6000 series, respectively). They included one-quarter horsepower models, one-third horsepower machines (Styles 5113, 6113F and 6213F), one-half horsepower types (Styles 5117, 6117F and 6217F) and one horsepower choppers (Styles 5128, 6128F and 6228F). Prices in 1926 varied from $180 to $375. Three years after this photograph was taken, the Dayton Scale Company became an IBM division, and was sold to the Hobart Manufacturing Company in 1934.", altImage: "Dayton Meat Chopper", price: 4599, rating: 0, id: 1, image: "meat-chopper")
     
-    var reviewList: [Review] = [
+  /*  var reviewList: [Review] = [
         Review(itemID: 12401, itemRating: 4, comments: "Nice Product Easy to Use, Bad Packing", email: "cptchouk@us.ibm.com", name: "Chris Tchoukaleff", id: 1),
         Review(itemID: 12401, itemRating: 3, comments: "Product decent but not great", email: "gangchen@us.ibm.com", name: "Gang Chen", id: 2)
-    ]
+    ] */
+    var reviewList: [Review] = []
     
     @IBAction func unwindToMenu(segue: UIStoryboardSegue) {}
     
@@ -58,7 +61,54 @@ class DetailViewController: UIViewController, UINavigationControllerDelegate, UI
         reviewTable.delegate = self
         reviewTable.dataSource = self
         
+        let appDelegate : AppDelegate = AppDelegate().sharedInstance()
+        //let appDelegate : AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        //let userDefaults = appDelegate.userDefaults as? NSUserDefaults
+        
+        var reviewRestUrl: String = appDelegate.userDefaults.objectForKey("reviewRestUrl") as! String
+        reviewRestUrl += "/api/reviews?filter={\"where\":{\"itemId\":\(self.item.id)}}"
+        let finalreviewUrl = reviewRestUrl.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!
+        //reviewRestUrl += "/api/reviews"
+        print("Review REST endpoint is : \(reviewRestUrl)")
+        //Set up REST framework
+        self.http = Http()
+        self.listReviews(finalreviewUrl, parameters: nil)
+        
     }
+    
+    func listReviews(url: String, parameters: [String: AnyObject]?) {
+        print("calling listReviews")
+        self.http.request(.GET, path: url, parameters: parameters, completionHandler: {(response, error) in
+            // handle response
+            if (error != nil) {
+                print("Error \(error!.localizedDescription)")
+            } else {
+                //print("Successfully invoked! \(response)")
+                
+                do {
+                    
+                    
+                    
+                    let resArry = response as! NSArray
+                    //let descrip: String = resArry![0].objectForKey("description") as! String
+                    for respItem in resArry {
+                        
+                       let newReview = Review(itemID: respItem.objectForKey("itemId") as! Int, itemRating: respItem.objectForKey("rating") as! Int, comments: respItem.objectForKey("comment") as! String, email: respItem.objectForKey("reviewer_email") as! String, name: respItem.objectForKey("reviewer_name") as! String, id: respItem.objectForKey("id") as! Int)
+                        
+                       self.reviewList.append(newReview)
+                        //print(newItem)
+                       self.reviewTable.reloadData()
+                    }
+                    
+                }
+                catch {
+                    print(error)
+                }
+                
+            }
+        })
+    }
+
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
